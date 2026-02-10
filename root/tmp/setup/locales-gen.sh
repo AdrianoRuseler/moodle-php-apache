ce3c456087d884a784739f5d0eb688525f554d89
Adriano Ruseler <adrianoruseler@gmail.com>
2026-02-10 12:55:29 -0300

    Update locales-gen.sh

--- root/tmp/setup/locales-gen.sh
+++ root/tmp/setup/locales-gen.sh
@@ -14,7 +14,13 @@ apt-get install -y --no-install-recommends apt-transport-https \
 echo "Installing UTF-8 locales"
 
 # Generate the locales configuration for all possible UTF-8 locales.
-grep UTF-8 /usr/share/i18n/SUPPORTED > /etc/locale.gen
+#grep UTF-8 /usr/share/i18n/SUPPORTED > /etc/locale.gen
+#locale-gen
+
+update-locale LANG=C.UTF-8 LC_MESSAGES=POSIX && DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
+echo 'en_AU.UTF-8 UTF-8' >> /etc/locale.gen
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+echo 'pt_BR.UTF-8 UTF-8' >> /etc/locale.gen
 locale-gen
 
 # Keep our image size down..
