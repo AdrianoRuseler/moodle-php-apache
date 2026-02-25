6bfd615b5927f13b528c32f52319bce82e7d9f5a
Adriano Ruseler <adrianoruseler@gmail.com>
2026-02-25 09:40:06 -0300

    Cleanup!

--- scripts/init-plugins-release.sh
+++ scripts/init-plugins-release.sh
@@ -89,6 +89,18 @@ EOF
     mdlver=$(cat $APACHE_DOC/version.php | grep '$release' | cut -d\' -f 2) # Gets Moodle Version
     php $MDLHOME/admin/cli/install_database.php --agree-license --fullname="Moodle $mdlver" --shortname="Moodle $mdlver" --adminpass="M@0dl3ing" --adminemail="admin@host.docker.internal"
 
+    echo "ℹ️ Admin settings configuration..."
+
+    echo "📂 System paths..."
+    #php cfg.php --name=<configname> [--component=<componentname>] --set=<value>
+    php $MDLHOME/admin/cli/cfg.php --name=pathtophp --set=/usr/local/bin/php
+    php $MDLHOME/admin/cli/cfg.php --name=pathtodu --set=/usr/bin/du
+    php $MDLHOME/admin/cli/cfg.php --name=pathtogs --set=/usr/bin/gs
+    php $MDLHOME/admin/cli/cfg.php --name=pathtopdftoppm --set=/usr/bin/pdftoppm
+    php $MDLHOME/admin/cli/cfg.php --name=pathtodot --set=/usr/bin/dot
+    php $MDLHOME/admin/cli/cfg.php --name=aspellpath --set=/usr/bin/aspell
+    php $MDLHOME/admin/cli/cfg.php --name=pathtopython --set=/usr/bin/python3
+
 else
     echo "ℹ️ Moodle code already exists, skipping clone."
     # Optional: Run composer install again in case dependencies changed
@@ -106,9 +118,9 @@ php $MDLHOME/admin/cli/upgrade.php --non-interactive
 php $MDLHOME/admin/cli/maintenance.php --disable
 
 # Run cron to finalize plugin installation and any pending tasks
-echo "🆙 Running cron -> null..."
+#echo "🆙 Running cron -> null..."
 #php $MDLHOME/admin/cli/cron.php >/dev/null
-php $MDLHOME/admin/cli/cron.php
+#php $MDLHOME/admin/cli/cron.php
 
 # Run checks to verify everything is set up correctly
 # php $MDLHOME/admin/cli/checks.php
@@ -118,16 +130,6 @@ php $MDLHOME/admin/cli/cron.php
 # MDLHOME="/var/www/html"
 #php $MDLHOME/admin/cli/cfg.php --help
 #php $MDLHOME/admin/cli/cfg.php --json
-echo "ℹ️ Admin settings configuration..."
 
-echo "📂 System paths..."
-#php cfg.php --name=<configname> [--component=<componentname>] --set=<value>
-php $MDLHOME/admin/cli/cfg.php --name=pathtophp --set=/usr/local/bin/php
-php $MDLHOME/admin/cli/cfg.php --name=pathtodu --set=/usr/bin/du
-php $MDLHOME/admin/cli/cfg.php --name=pathtogs --set=/usr/bin/gs
-php $MDLHOME/admin/cli/cfg.php --name=pathtopdftoppm --set=/usr/bin/pdftoppm
-php $MDLHOME/admin/cli/cfg.php --name=pathtodot --set=/usr/bin/dot
-php $MDLHOME/admin/cli/cfg.php --name=aspellpath --set=/usr/bin/aspell
-php $MDLHOME/admin/cli/cfg.php --name=pathtopython --set=/usr/bin/python3
 
 echo "✅ Moodle Init completed!!!"
